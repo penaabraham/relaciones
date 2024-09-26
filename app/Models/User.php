773445fa2a41c1e1af8ce4fc824cbe99c54680fb
penaabraham <penaabraham@micorreo.upp.edu.mx>
2024-09-25 18:13:52 -0600

    6 - Relación muchos a muchos | Eloquent ORM | Rimorsoft Online
    Tabla intermedia: group_user

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Level::class);
     }
+
+    public function groups()
+    {
+        return $this->belongsToMany(Group::class)->withTimestamps();
+    }
 }
