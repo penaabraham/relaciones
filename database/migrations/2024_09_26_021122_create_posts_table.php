a60abe853c4e59ce92b00df1d391b12d1ea0ae13
penaabraham <penaabraham@micorreo.upp.edu.mx>
2024-09-25 21:38:45 -0600

    9 - Todas las tablas: Category, Tag, Taggables, Post y mas | Rimorsoft Online
    RELACIONES POLIMORFICAS (...able).
    
    1 comentario puede escribirse en un video o en un post, por ende, no se pueden agregar columnas adicionales como "post_id" o  "video_id".
    
    Para ello se utiliza el metodo "morph", y el nombre del campo seria "commentable".
    
    morphs, en este caso, va a generar 2 campos:
    
    - 1 haciendo referencia al id,
    - 1 haciendo referencia a la entidad
    
    able = "capacidad de"

---
+++ database/migrations/2024_09_26_021122_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->bigInteger('category_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+
+            $table->string('name');
+
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
