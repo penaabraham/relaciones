3aeed168e3af7bb7877888cfa98940e177771598
penaabraham <penaabraham@micorreo.upp.edu.mx>
2024-09-25 19:41:29 -0600

    7 - Edición de vista Welcome | Rimorsoft Online

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $users = App\User::get();
+
+    return view('welcome', ['users' => $users]);
 });
