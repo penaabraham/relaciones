a52be431556525e070746b225b95fb6d75e0376d
penaabraham <penaabraham@micorreo.upp.edu.mx>
2024-09-25 20:06:45 -0600

    8 - Relación uno a uno a través de... | Eloquent ORM | Rimorsoft Online
    locations:
    $table->foreign('profile_id')->references('id')->on('profiles')
                    ->onDelete('cascade')
                    ->onUpdate('cascade');
    
    Profile:
     public function location()
        {
            return $this->hasOne(Location::class);
        }
    
    User:
    public function location()
        {
            return $this->hasOneThrough(Location::class, Profile::class);
        }

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -5,12 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Profile extends Model
+class Location extends Model
 {
     use HasFactory;
-
-    public function location()
-    {
-        return $this->hasOne(Location::class);
-    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,10 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Group::class)->withTimestamps();
     }
+
+    public function location()
+    {
+        return $this->hasOneThrough(Location::class, Profile::class);
+    }
+
 }
