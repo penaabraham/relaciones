0092a40cba217d4abbdf38b1dff34e425ecb3d59
penaabraham <penaabraham@micorreo.upp.edu.mx>
2024-09-26 17:29:24 -0600

    20 - Seeder de Posts y Videos (Category, Tags & Comments) | Rimorsoft Online

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ use App\Models\User;
 use App\Models\Profile;
 use App\Models\Location;
 use App\Models\Image;
+use App\Models\Category;
+use App\Models\Tag;
+use App\Models\Post;
+use App\Models\Comment;
+use App\Models\Video;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -36,6 +41,42 @@ class DatabaseSeeder extends Seeder
             $user->image()->save(Image::factory()->make([
                 'url' => 'https://lorempixel.com/90/90/' // URL ficticia
             ]));
+        });
+
+        // Crea 4 categorías
+        Category::factory()->count(4)->create();
+
+        // Crea 12 etiquetas
+        Tag::factory()->count(12)->create();
+
+        // Crea 40 publicaciones con imágenes, etiquetas y comentarios
+        Post::factory()->count(40)->create()->each(function ($post) {
+            // Asocia una imagen a la publicación
+            $post->image()->save(Image::factory()->make());
+
+            // Asocia etiquetas a la publicación
+            $post->tags()->attach($this->array(rand(1, 12)));
+
+            // Agrega comentarios a la publicación
+            $number_comments = rand(1, 6);
+            for ($i = 0; $i < $number_comments; $i++) {
+                $post->comments()->save(Comment::factory()->make());
+            }
+        });
+
+        // Crea 40 videos con imágenes, etiquetas y comentarios
+        Video::factory()->count(40)->create()->each(function ($video) {
+            // Asocia una imagen al video
+            $video->image()->save(Image::factory()->make());
+
+            // Asocia etiquetas al video
+            $video->tags()->attach($this->array(rand(1, 12)));
+
+            // Agrega comentarios al video
+            $number_comments = rand(1, 6);
+            for ($i = 0; $i < $number_comments; $i++) {
+                $video->comments()->save(Comment::factory()->make());
+            }
         });
 
         // Crea un usuario de prueba específico
